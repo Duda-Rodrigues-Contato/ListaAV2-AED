046391014f7f5fca72d3dec31fca1d8286440e00
Duda Rodrigues <147636488+Duda-Rodrigues-Contato@users.noreply.github.com>
2025-11-25 11:35:28 -0300

    Finalizei a questao 17 e preparei o arquivo para a questao 18.

--- Parte 2 - Algoritmos de Ordenação/Selection Sort/questao17.c
+++ Parte 2 - Algoritmos de Ordenação/Selection Sort/questao17.c
@@ -1,11 +1,47 @@
-// 
+// Conte trocas: {5, 4, 3, 2, 1, 6, 7, 8, 9, 10}
 
 #include <stdio.h>
 
+void selectionSort(int arr[], int n) {
+
+    int cont = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+
+        int min_idx = i;
+
+        for (int j = i + 1; j < n; j++) {
+
+            if (arr[j] < arr[min_idx])
+            min_idx = j;
+
+        }
+
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+        cont++;
+
+    }
+
+    printf("Total de trocas: %d\n", cont);
+
+}
+
+
 int main() {
 
+    int arr[] = {5, 4, 3, 2, 1, 6, 7, 8, 9, 10};
+    int tam = 10;
 
+    selectionSort(arr, tam);
+
+    printf("Array ordenado: \n");
+
+    for(int i = 0; i < tam; i++)
+
+        printf("%d ", arr[i]);
 
     return 0;
-    
+
 }
---
+++ Parte 2 - Algoritmos de Ordenação/Selection Sort/questao18.c
@@ -0,0 +1,9 @@
+// Ordem decrescente: {42, 33, 60, 15, 75, 12, 25, 90, 1, 6}
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+
+}
